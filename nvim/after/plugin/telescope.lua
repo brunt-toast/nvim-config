19b22caa8bf12b1506599fb6a9635d8c818bb834
Josh Brunton <josh.brunton@proton.me>
2023-12-20 14:08:31 +0000

    New telescope keymatch matches VC# 2005

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,6 +3,7 @@ local builtin = require('telescope.builtin')
 -- vim.g.mapleader = " "
 vim.keymap.set('n', ':ts', builtin.find_files, {}) -- telescope
 vim.keymap.set('n', ':gs', builtin.git_files, {}) -- gitscope
+vim.keymap.set('n', '<C-,>', builtin.find_files, {}) -- telescope like VS VC# 2005
 
 -- make the telescope window transparent
 vim.api.nvim_set_hl(0, "FloatBorder", {bg="#000000", fg="#ffffff" })
